83d71d9fb092f95d1db7cbc864f28b55d886d406
Yoofa1 <efimshevnin1234@gmail.com>
2025-11-21 21:41:30 +0300

    дз heellllooouu

---
+++ HomeWorkOne/HomeWork.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Greeting: ");
+        string greetingWord = Console.ReadLine();
+
+        Console.WriteLine(GetAnswer(greetingWord));
+    }
+
+    static string GetAnswer(string word)
+    {
+        string example = "hello";
+        int indexExample = 0;
+
+        foreach (char c in word)
+        {
+            if (c == example[indexExample])
+            {
+                indexExample++;
+            }
+
+            if (indexExample == example.Length)
+            {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+}
