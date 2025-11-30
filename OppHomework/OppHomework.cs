aef277a8665cec59173b448f4254ae7a329238c6
Yoofa1 <efimshevnin1234@gmail.com>
2025-11-30 15:18:14 +0300

    add a new method "DisplayItemsInfo" in namespace "OppHomeworkOne"

--- OppHomework/OppHomework.cs
+++ OppHomework/OppHomework.cs
@@ -52,6 +52,77 @@
                     }
                 }
             });
+
+            DisplayItemsInfo( items );
+        }
+
+        static void DisplayItemsInfo(List<IOrderProcessingSystem> items, string indent = "|| ")
+        {
+            foreach (var item in items)
+            {
+                switch (item)
+                {
+                    case Customer customer:
+                        Console.WriteLine("=================");
+                        Console.WriteLine($"{indent}Customer:");
+                        Console.WriteLine($"{indent}  ID: {customer.Id}");
+                        Console.WriteLine($"{indent}  Name: {customer.Name}");
+                        Console.WriteLine($"{indent}  Balance: {customer.Balance}");
+                        if (customer.Items?.Count > 0)
+                        {
+                            Console.WriteLine($"{indent}  Orders:");
+                            DisplayItemsInfo(customer.Items, indent + " ");
+                        }
+                        break;
+
+                    case Order order:
+                        Console.WriteLine("=================");
+                        Console.WriteLine($"{indent}Order:");
+                        Console.WriteLine($"{indent}  ID: {order.Id}");
+                        Console.WriteLine($"{indent}  Date: {order.OrderDate}");
+                        if (order.Items?.Count > 0)
+                        {
+                            Console.WriteLine($"{indent}  Payments:");
+                            DisplayItemsInfo(order.Items, indent + " ");
+                        }
+                        break;
+
+                    case Payment payment:
+                        Console.WriteLine("=================");
+                        Console.WriteLine($"{indent}Payment:");
+                        Console.WriteLine($"{indent}  ID: {payment.Id}");
+                        Console.WriteLine($"{indent}  Type: {payment.PaymentType}");
+                        Console.WriteLine($"{indent}  Date: {payment.PaymentDate}");
+                        if (payment.Items?.Count > 0)
+                        {
+                            Console.WriteLine($"{indent}  Carts:");
+                            DisplayItemsInfo(payment.Items, indent + " ");
+                        }
+                        break;
+
+                    case Cart cart:
+                        Console.WriteLine("=================");
+                        Console.WriteLine($"{indent}Cart:");
+                        Console.WriteLine($"{indent}  ID: {cart.Id}");
+                        if (cart.Items?.Count > 0)
+                        {
+                            Console.WriteLine($"{indent}  Products:");
+                            DisplayItemsInfo(cart.Items, indent + " ");
+                        }
+                        break;
+
+                    case Product product:
+                        Console.WriteLine("=================");
+                        Console.WriteLine($"{indent}Product:");
+                        Console.WriteLine($"{indent}  ID: {product.Id}");
+                        Console.WriteLine($"{indent}  Name: {product.Name}");
+                        Console.WriteLine($"{indent}  Price: {product.Price}");
+                        Console.WriteLine("+-+-+-+-+-+-+-+-+");
+
+                        break;
+                }
+                Console.WriteLine();
+            }
         }
     }
 
@@ -66,7 +137,6 @@
         public string Name { get; set; }
         public float Balance { get; set; }
         public List<IOrderProcessingSystem> Items { get; set; }
-
     }
 
     /// <summary>
@@ -101,7 +171,6 @@
         public string PaymentType { get; set; }     // by cart / non-cash
         public DateTime PaymentDate { get; set; }
         public List<IOrderProcessingSystem> Items { get; set; }
-
     }
 
     /// <summary>
@@ -113,7 +182,6 @@
         public int Id { get; set; }
         public DateTime OrderDate { get; set; }
         public List<IOrderProcessingSystem> Items { get; set; }
-
     }
 
     public interface IOrderProcessingSystem
